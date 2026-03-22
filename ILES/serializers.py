8a4b141446c92abb7ae64312ee5563cc795271b9
kyomuhendoziporah <kyomuhendoziporah@gmail.com>
2026-03-22 05:40:58 -0700

    My ILES backend files

---
+++ ILES/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import (CustomUser,Internship_Placement,Weekly_Log,Supervisor_Feedback,Academic_Supervisor_Feedback,Weighted_Score,Issue)
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id', 'username', 'first_name', 'last_name', 'email', 'role' ,'department', 'is_active', 'is_staff', 'student_number', 'staff_number',
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+
+class Internship_PlacementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Internship_Placement
+        fields = "__all__"
+
+class Weekly_LogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weekly_Log
+        exclude = ['submitted_at', 'created_at']  
+
+class Supervisor_FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supervisor_Feedback
+        fields = "__all__"
+
+class Academic_Supervisor_FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Academic_Supervisor_Feedback
+        fields = "__all__"
+
+class Weighted_ScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weighted_Score
+        fields = "__all__"
+
+class IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        exclude = ['created_at']
+        
