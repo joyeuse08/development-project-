cc74bb8a6fa6f8a0368c66f859786931aae5ae60
kyomuhendoziporah-design <kyomuhendoziporah@gmail.com>
2026-04-13 09:39:49 -0700

    Implement CustomUserAdmin for CustomUser model
    Added CustomUserAdmin class to manage CustomUser model in admin.

--- ILES/admin.py
+++ ILES/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser,Internship_Placement,Weekly_Log,Supervisor_Feedback,Academic_Supervisor_Feedback,Weighted_Score,Issues
+
+class CustomUserAdmin(UserAdmin):
+  model = CustomUser
+  list_display = ('username','role','department','staff_number','student_number')
+  fieldsets = UserAdmin.fieldets + (
+    ('Extra Info', {'fields':('role',)}),
+  )
+
+
 
 # Register your models here.
