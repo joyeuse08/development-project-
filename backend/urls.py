de48b4416224c4c2693c0353b8bda5cef1b58686
Clifford M. <mcliff149@gmail.com>
2026-04-17 20:14:42 +0300

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from ILES import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ILES.urls')),
-    path('search/', views.search_items, name='search'),
+   
 ]
