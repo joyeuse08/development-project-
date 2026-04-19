9e727fc055f459effe7909d68065f95df835a5b6
Akasinguza Deborah <akasinguzadeborah@gmail.com>
2026-04-19 16:10:42 -0700

    Fix router basename formatting in urls.py

--- ILES/urls.py
+++ ILES/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 router = DefaultRouter()
 router.register(r'users', views.CustomUserViewSet, basename='users')
-router.register(r'Internship_Placement', views.Internship_PlacementViewSet, basename= 'internship placement')
-router.register(r'Weekly_Log', views.Weekly_LogViewSet, basename= 'weeklylog')
-router.register(r'Supervisor_Feedback', views.Supervisor_FeedbackViewSet, basename= 'supervisorfeedback')
-router.register(r'Academic_Supervisor_Feedback', views.Academic_Supervisor_FeedbackViewSet, basename= 'academicsupervisor')
-router.register(r'Weighted_Score', views.Weighted_ScoreViewSet, basename= 'weightedscore')
-router.register(r'issues', views.IssueViewSet, basename= 'issue')  
+router.register(r'Internship_Placement', views.Internship_PlacementViewSet, basename='internship_placement')
+router.register(r'Weekly_Log', views.Weekly_LogViewSet, basename='weekly_log')
+router.register(r'Supervisor_Feedback', views.Supervisor_FeedbackViewSet, basename='supervisor_feedback')
+router.register(r'Academic_Supervisor_Feedback', views.Academic_Supervisor_FeedbackViewSet, basename='academic_supervisor')
+router.register(r'Weighted_Score', views.Weighted_ScoreViewSet, basename='weighted_score')
+router.register(r'issues', views.IssueViewSet, basename='issue')  
 
 
 urlpatterns = [
