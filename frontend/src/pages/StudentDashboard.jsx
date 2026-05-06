2c354d9c507899c08b5f8a68f405a48f4d981222
kyomuhendoziporah <kyomuhendoziporah@gmail.com>
2026-05-06 04:56:32 -0700

    added arrays

--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -13,17 +13,29 @@ function StudentDashboard() {
   useEffect(() => {
     // Fetch student's placements
     axios.get('/api/Internship_Placement/')
-      .then(res => setPlacements(res.data))
+      .then(res => {
+        const data = Array.isArray(res.data) ? res.data :
+                     Array.isArray(res.data.results) ? res.data.results : [];
+        setPlacements(data);
+      })
       .catch(err => console.log(err));
 
     // Fetch student's weekly logs
     axios.get('/api/Weekly_Log/')
-      .then(res => setLogs(res.data))
+      .then(res => {
+        const data = Array.isArray(res.data) ? res.data :
+                     Array.isArray(res.data.results) ? res.data.results : [];
+        setLogs(data);
+      })
       .catch(err => console.log(err));
 
     // Fetch notifications
     axios.get('/api/notifications/')
-      .then(res => setNotifications(res.data))
+      .then(res => {
+        const data = Array.isArray(res.data) ? res.data :
+                     Array.isArray(res.data.results) ? res.data.results : [];
+        setNotifications(data);
+      })
       .catch(err => console.log(err));
   }, []);
 
