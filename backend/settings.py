7f3316b7f4b74217fbbb28a6cb41178e81294d5c
Deborah <akasinguzadeborah@gmail.com>
2026-05-08 07:50:13 -0700

    My local changes

--- backend/settings.py
+++ backend/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'Glokknine/17',
+        'PASSWORD': 'food',
         'HOST': 'localhost',
         'PORT': '5432',
     }
