004ca76377b8fa02d6341392221a6ea45df0f492
kyomuhendoziporah <kyomuhendoziporah@gmail.com>
2026-05-08 04:52:26 -0700

    made changes

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 # Exit on error
 set -o errexit
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR"
 
 pip install -r requirements.txt
 python backend/manage.py collectstatic --noinput
