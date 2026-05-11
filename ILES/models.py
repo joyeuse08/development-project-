0fb694db313b43c45b4b604ec88ecd37793143d0
kyomuhendoziporah-design <kyomuhendoziporah@gmail.com>
2026-05-11 02:47:20 -0700

    Change supervisor ForeignKey on_delete behavior

--- ILES/models.py
+++ ILES/models.py
@@ -97,7 +97,7 @@ class Student_log(models.Model):
         ('rejected', 'Rejected'),   
     ] 
     student = models.ForeignKey(Internship_Placement, on_delete=models.CASCADE, related_name='logs')
-    supervisor = models.ForeignKey(CustomUser, on_delete=models.CASCADE,null =True, blank =True, related_name='student_logs', limit_choices_to={'role': 'workplace'})
+    supervisor = models.ForeignKey(CustomUser, on_delete=models.SET_NULL,null =True, blank =True, related_name='student_logs', limit_choices_to={'role': 'workplace'})
     title = models.CharField(max_length=255,null=True, blank=True)
     date=models.DateField()
     description = models.TextField()
