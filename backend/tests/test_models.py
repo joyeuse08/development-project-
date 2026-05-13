e0d6bcc27570d9060b8d055da073e65b1cb1d178
Joyeuse <joyeuseniyotwizera23@gmail.com>
2026-05-13 09:46:36 +0300

    Update test_models.py

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -1,1 +1,42 @@
 
+import pytest
+from internship.models import CustomUser, WeeklyLog
+
+@pytest.mark.django_db
+def test_create_user():
+    user = CustomUser.objects.create_user(
+        username="joy",
+        password="1234",
+        role="student"
+    )
+    assert user.role == "student"
+
+
+@pytest.mark.django_db
+def test_create_log():
+    user = CustomUser.objects.create_user(
+        username="joy",
+        password="1234"
+    )
+
+    log = WeeklyLog.objects.create(
+        student=user,
+        activities="Worked on React"
+    )
+
+    assert log.status == "draft"
+
+
+@pytest.mark.django_db
+def test_week_number():
+    user = CustomUser.objects.create_user(
+        username="joy"
+    )
+
+    log = WeeklyLog.objects.create(
+        student=user,
+        week=1,
+        activities="Testing"
+    )
+
+    assert log.week == 1
