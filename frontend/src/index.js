e9f54f1d59356a4159b92f31d5a6d4f060133fcf
Deborah <akasinguzadeborah@gmail.com>
2026-05-18 11:16:57 -0700

    Fix duplicate code in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,9 @@
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import "./index.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -22,5 +11,4 @@ root.render(
       <App />
     </AuthProvider>
   </React.StrictMode>
-
 );
