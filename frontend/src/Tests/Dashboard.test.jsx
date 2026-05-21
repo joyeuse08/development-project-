b0b2cc4799343d00914f649bea4d2da8dc20cf9d
Joyeuse <joyeuseniyotwizera23@gmail.com>
2026-05-21 19:49:38 +0300

    Add Dashboard component test

--- frontend/src/Tests/Dashboard.test.jsx
+++ frontend/src/Tests/Dashboard.test.jsx
@@ -1,1 +1,14 @@
+import { render, screen } from "@testing-library/react"
 
+function Dashboard() {
+   return <h1>Dashboard</h1>
+}
+
+test("renders dashboard", () => {
+
+   render(<Dashboard />)
+
+   expect(
+      screen.getByText("Dashboard")
+   ).toBeInTheDocument()
+})
