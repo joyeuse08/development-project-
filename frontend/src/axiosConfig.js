15f9eb0ff00ec94d24d17a4d3630956959f5e04e
kyomuhendoziporah <kyomuhendoziporah@gmail.com>
2026-06-08 09:11:06 -0700

    added render url

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '',
+  baseURL: process.env.REACT_APP_API_URL || 'https://iles-backend-ltp6.onrender.com',
 });
 
 export default api;
