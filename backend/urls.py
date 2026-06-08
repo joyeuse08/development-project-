d3d8f3f97ffa0e17a3eb6d79e6e44fe9d4cbfa94
Clifford M. <mcliff149@gmail.com>
2026-06-08 21:19:12 +0300

    Add dashboard view to URL patterns

--- backend/urls.py
+++ backend/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from ILES.views import dashboard_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ILES.urls')),
-   
+    path('dashboard/', dashboard_view, name='dashboard'), 
 ]
 
 if settings.DEBUG:
