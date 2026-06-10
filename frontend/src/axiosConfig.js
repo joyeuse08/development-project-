5b8eb4e0f5194d8167e4ac7fa93dd3e7f352a5f9
kyomuhendoziporah <kyomuhendoziporah@gmail.com>
2026-06-10 03:49:14 -0700

    added  token to axios

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'https://iles-backend-ltp6.onrender.com',
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');  
+  if (token) {
+    config.headers['Authorization'] = `Token ${token}`;
+  }
+  return config;
+});
+
 export default api;
