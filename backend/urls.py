71682b407ac13237be3cbf54437007b222761de9
Clifford M. <mcliff149@gmail.com>
2026-06-10 09:59:24 +0300

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -31,3 +31,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    re_path(r'^(?!api/|admin/).*$', TemplateView.as_view(template_name='index.html'), name='react-app'),
+]
