c2acd13648a3518e8eb689b9b968962e02bf9dfd
kyomuhendoziporah <kyomuhendoziporah@gmail.com>
2026-06-11 00:25:35 -0700

    settings and app.js

--- backend/settings.py
+++ backend/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'build',]
+STATICFILES_DIRS = []
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL = "ILES.CustomUser"
