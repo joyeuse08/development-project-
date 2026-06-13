9d6c4d63c11334a6d3e048e2ed8fb5c2c21ae4f7
kyomuhendoziporah <kyomuhendoziporah@gmail.com>
2026-06-13 04:18:29 -0700

    increased page size

--- backend/settings.py
+++ backend/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
 }
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
